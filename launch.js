22705ab91f4fc67714e3f1ec069fde33613c5d43
sglowa <sglowa@saic.edu>
2018-02-24 19:41:50 -0600

    finished launch.js

--- launch.js
+++ launch.js
@@ -1,10 +1,9 @@
-var exec = require("child_process").exec;
+var exec = require("child_process").execFile;
 
+var gamePath = "C:/Program\ Files\ \(x86\)/Steam/steamapps/common/Counter-Strike\ Source/hl2.exe"; 
+var gameFlags = ["-game", "cstrike", "-dev", "-windowed", "-console"];
 
-var gamePath = "C:/Program Files (x86)/Steam/steamapps/common/Counter-Strike Source/hl2.exe" 
-
-
-// exec('/c/Program\ Files\ \(x86\)/Steam/steamapps/common/Counter-Strike\ Source/hl2.exe -game cstrike -dev -windowed -console -sw +sv_lan 1 +sv_cheats 1');
-exec('"C:/Program Files (x86)/Steam/steamapps/common/Counter-Strike Source/hl2.exe" -game cstrike -dev -windowed -console')
-
-console.log("czesc");
+exec(gamePath,gameFlags, function (err,data){
+	console.log(err);
+	console.log(data.toString());
+});
